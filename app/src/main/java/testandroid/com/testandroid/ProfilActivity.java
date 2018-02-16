84350e028482c37550928eefe782685e67f8d5ae
NesrineMD <nesrinemdaini@gmail.com>
2018-02-16 15:04:54 +0100

    login +register + change Email+ change Password 1

--- app/src/main/java/testandroid/com/testandroid/ProfilActivity.java
+++ app/src/main/java/testandroid/com/testandroid/ProfilActivity.java
@@ -3,11 +3,11 @@ package testandroid.com.testandroid;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class accueilActivity extends AppCompatActivity {
+public class ProfilActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_accueil);
+        setContentView(R.layout.activity_profil);
     }
 }
